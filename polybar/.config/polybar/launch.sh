adcafd3feabf15ce0ab4e00880757c474cccfde5
banbeucmas <banbeucams@gmail.com>
2022-06-04 04:19:32 +0900

    Mpd music bar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,6 +4,7 @@ polybar -r main-bar-left &
 polybar -r main-bar-right & 
 polybar -r main-bar-center &
 polybar -r music-player & sleep 1
+#polybar -r music-player-center 
 
 #Hide the music player bar
 polybar-msg cmd hide
