e35aec9c03de1c1f9cbee760ffb727bb2606ebcb
Banbeucmas <banbeucams@gmail.com>
2022-10-04 01:54:15 +0000

    Laptop configuration

--- config/polybar/.config/polybar/launch.sh
+++ config/polybar/.config/polybar/launch.sh
@@ -3,9 +3,9 @@
 polybar -r main-bar-left & 
 polybar -r main-bar-right & 
 polybar -r main-bar-center &
-polybar -r music-player & sleep 1
+# polybar -r music-player & sleep 1
 #polybar -r music-player-center 
 
 #Hide the music player bar
-polybar-msg cmd hide
-bspc config -m focused bottom_padding $((`bspc config -m focused bottom_padding` - 42 ))
+# polybar-msg cmd hide
+# bspc config -m focused bottom_padding $((`bspc config -m focused bottom_padding` - 42 ))
